033408cdfd805a7883805fd0b3a8efc06d372649
Noatz <ethancatzel@gmail.com>
2016-11-05 14:31:43 +1100

    DailyTimetable script created

--- GPI/GPI/Program.cs
+++ GPI/GPI/Program.cs
@@ -16,7 +16,7 @@ namespace GPI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
